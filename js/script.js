952c6814f579a603b7ef1eb06f1f010ed34b7834
Brian Bradley <brianbradley@brians-air.attlocal.net>
2016-03-18 17:50:50 -0500

    change to original

--- js/script.js
+++ js/script.js
@@ -11,43 +11,43 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetValue = $("#street").val();
-    var cityVal =    $("#city").val();
-    var address = streetValue +  cityValue;
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-    //load streetview
-    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class = "bgimg"  src = "' + streetViewUrl + '">');
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
 
 
-    //   Ny Times Article Search
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    $.getJSON(nytimesUrl, function(data){
 
-     var nytimesURL =  'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityValue + '&sort=newest&api-key=e7230f27d3ae1a10f2c601cd3b93a0e3:8:74740630';
-     $.getJSON( nytimesURL, function( data ) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
-        $nytHeaderElem.text('New York Times Articles About ' + cityValue);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
 
-        articles = data.response.docs;  //response.docs is in the Response section JS
-        for (var i =0; i < articles.length; i++) {
-            var article = articles [i];
-            $nytElem.append('<li class ="article">' +
-             '<a href = "'+article.web_url+'">' + article.headline.main+'</a>' +
-                 '<p>' + article.snippet + '</p>' +
-                 '</li>');
-         };
-
-      }).error(function(e) {
-        $nytHeaderElem.text("New Your Times Could Not be Loaded");
-      });
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
 
 
 
-     // Load Wikipedia
-       var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
