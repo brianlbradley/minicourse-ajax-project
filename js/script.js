d089a138a2c4dc973924b917f47107276cb497bb
Brian Bradley <brianbradley@brians-air.attlocal.net>
2016-03-18 20:08:59 -0500

    Corrected variable reference to cityStr

--- js/script.js
+++ js/script.js
@@ -11,22 +11,21 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+    var streetStr = $("#street").val();
+    var cityStr =    $("#city").val();
+    var address = streetStr+ ',' + cityStr;
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-    // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    //load streetview
+   var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
 
+    //   Ny Times Article Search
 
-    // load nytimes
-    // obviously, replace all the "X"s with your own API key
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+       var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=e7230f27d3ae1a10f2c601cd3b93a0e3:8:74740630';
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -46,8 +45,8 @@ function loadData() {
 
 
 
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+     // Load Wikipedia
+       var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
